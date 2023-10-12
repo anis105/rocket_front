f7fb55ab64d13fcb661fa3aef2227f13ec807cc8
Joey <qgao@umd.edu>
2023-10-12 01:00:30 -0400

    Update App.js
    Add New Tasks quickly
    Delete Tasks
    Complete Tasks

--- src/App.js
+++ src/App.js
@@ -1,81 +1,74 @@
 import React, { useState } from 'react';
 
 function TodoList() {
-  const [todos, setTodos] = useState([
-    { id: 1, date: '2023-10-12', task: 'Math Homework', completed: false },
-    { id: 2, date: '2023-10-12', task: 'Physics Assignment', completed: false },
-    { id: 3, date: '2023-10-13', task: 'History Reading', completed: false }
-  ]);
+    const [todos, setTodos] = useState([
+        { id: 1, date: '2023-10-12', task: 'Math Homework', completed: false },
+        { id: 2, date: '2023-10-12', task: 'Physics Assignment', completed: false },
+        { id: 3, date: '2023-10-13', task: 'History Reading', completed: false }
+    ]);
 
-  const [newTask, setNewTask] = useState('');
-  const [selectedDate, setSelectedDate] = useState('');
+    const [newTask, setNewTask] = useState('');
+    const [selectedDate, setSelectedDate] = useState('');
 
-  const groupedTodos = todos.reduce((acc, todo) => {
-    if (!acc[todo.date]) {
-      acc[todo.date] = [];
-    }
-    acc[todo.date].push(todo);
-    return acc;
-  }, {});
+    const handleAddTask = () => {
+        if (newTask.trim() && selectedDate) {
+            setTodos([...todos, {
+                id: Date.now(),
+                date: selectedDate,
+                task: newTask,
+                completed: false
+            }]);
+            setNewTask('');
+            setSelectedDate('');
+        }
+    };
 
-  const handleAddTask = () => {
-    if (newTask.trim() && selectedDate) {
-      const newTodo = {
-        id: Date.now(), // simplistic ID generation
-        date: selectedDate,
-        task: newTask,
-        completed: false
-      };
-      setTodos([...todos, newTodo]);
-      setNewTask('');
-      setSelectedDate('');
-    }
-  };
-
-  const handleDeleteTask = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
-  const handleToggleCompleted = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo));
-  };
-
-  return (
-      <div>
+    return (
         <div>
-          <input
-              type="date"
-              value={selectedDate}
-              onChange={(e) => setSelectedDate(e.target.value)}
-          />
-          <input
-              type="text"
-              placeholder="New Task"
-              value={newTask}
-              onChange={(e) => setNewTask(e.target.value)}
-          />
-          <button onClick={handleAddTask}>Add Task</button>
-        </div>
-        {Object.entries(groupedTodos).map(([date, todosForDate]) => (
-            <div key={date}>
-              <h2>{date}</h2>
-              <ul>
-                {todosForDate.map(todo => (
-                    <li key={todo.id}>
-                                <span
+            {/* The "Add New Task" section */}
+            <div>
+                <input 
+                    type="date" 
+                    value={selectedDate} 
+                    onChange={(e) => setSelectedDate(e.target.value)} 
+                />
+                <input 
+                    type="text" 
+                    placeholder="What's next on your list?"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                />
+                <button onClick={handleAddTask}>Bring it on!</button>
+            </div>
+
+            {/* Displaying the tasks grouped by date */}
+            {Object.entries(todos.reduce((grouped, todo) => {
+                (grouped[todo.date] = grouped[todo.date] || []).push(todo);
+                return grouped;
+            }, {})).map(([date, todosForDate]) => (
+                <div key={date}>
+                    <h2>{date}</h2>
+                    <ul>
+                        {todosForDate.map(todo => (
+                            <li key={todo.id}>
+                                <span 
                                     style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                                    onClick={() => handleToggleCompleted(todo.id)}
+                                    onClick={() => setTodos(todos.map(t => 
+                                        t.id === todo.id ? {...t, completed: !t.completed} : t
+                                    ))}
                                 >
                                     {todo.task}
                                 </span>
-                      <button onClick={() => handleDeleteTask(todo.id)}>Delete</button>
-                    </li>
-                ))}
-              </ul>
-            </div>
-        ))}
-      </div>
-  );
+                                <button onClick={() => setTodos(todos.filter(t => t.id !== todo.id))}>
+                                    I'm done with this!
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default TodoList;
