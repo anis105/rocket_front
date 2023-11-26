2b62038aee0360abb525ab8cfe271d37e06e6981
Qeelin <qchee7@gmail.com>
2023-11-26 16:43:37 +0000

    Todolist - CURD - search - priority management
    login
    - login module

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import './App.css';
 function TodoList() {
-    const [todos, setTodos] = useState([
-        { id: 1, date: '2023-10-12', task: 'Math Homework', completed: false },
-        { id: 2, date: '2023-10-12', task: 'Physics Assignment', completed: false },
-        { id: 3, date: '2023-10-13', task: 'History Reading', completed: false }
-    ]);
+    const initialTodos = [
+        { id: 1, date: '2023-11-25', task: 'Math Homework', completed: false, priority: 'High', notes: 'Chapter 5 and 6' },
+        { id: 2, date: '2023-11-26', task: 'Physics Assignment', completed: true, priority: 'Medium', notes: 'Experiment report' },
+        { id: 3, date: '2023-11-27', task: 'History Reading', completed: false, priority: 'Low', notes: 'Read pages 101-120' }
+    ];
+
+    const [todos, setTodos] = useState(() => {
+        const savedTodos = localStorage.getItem('todos');
+        return savedTodos ? JSON.parse(savedTodos) : initialTodos;
+    });
 
     const [newTask, setNewTask] = useState('');
+    const [newPriority, setNewPriority] = useState('Medium');
+    const [newNotes, setNewNotes] = useState('');
     const [selectedDate, setSelectedDate] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showLogin, setShowLogin] = useState(false);  // State for controlling login modal visibility
+
+    const handleLogin = () => {
+        setShowLogin(!showLogin); // Toggle login modal visibility
+    };
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
 
     const handleAddTask = () => {
         if (newTask.trim() && selectedDate) {
@@ -16,49 +32,100 @@ function TodoList() {
                 id: Date.now(),
                 date: selectedDate,
                 task: newTask,
-                completed: false
+                completed: false,
+                priority: newPriority,
+                notes: newNotes
             }]);
             setNewTask('');
+            setNewPriority('Medium');
+            setNewNotes('');
             setSelectedDate('');
         }
     };
 
+    const filteredTodos = searchTerm.length === 0 ? todos : todos.filter(todo =>
+        todo.task.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const groupedTodos = filteredTodos.reduce((grouped, todo) => {
+        (grouped[todo.date] = grouped[todo.date] || []).push(todo);
+        return grouped;
+    }, {});
+
     return (
-        <div>
-            {/* The "Add New Task" section */}
+        <div className="TodoList-container">
+            {/* Login Tab */}
+            <div className="login-tab" onClick={handleLogin}>
+                Log In
+            </div>
+            {/* Login Modal */}
+            {showLogin && (
+                <div className="login-modal">
+                    <div className="login-content">
+                        <span className="close" onClick={() => setShowLogin(false)}>&times;</span>
+                        <h2>Login</h2>
+                        <h3>Welcome Terps!</h3>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button onClick={() => console.log('Perform login')}>Login</button>
+                    </div>
+                </div>
+            )}
+            {/* Add New Task Section */}
             <div>
-                <input 
-                    type="date" 
-                    value={selectedDate} 
-                    onChange={(e) => setSelectedDate(e.target.value)} 
+                <input
+                    type="date"
+                    value={selectedDate}
+                    onChange={(e) => setSelectedDate(e.target.value)}
                 />
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     placeholder="What's next on your list?"
                     value={newTask}
                     onChange={(e) => setNewTask(e.target.value)}
                 />
+                <select
+                    value={newPriority}
+                    onChange={(e) => setNewPriority(e.target.value)}
+                >
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                </select>
+                <textarea
+                    placeholder="Add notes here"
+                    value={newNotes}
+                    onChange={(e) => setNewNotes(e.target.value)}
+                />
                 <button onClick={handleAddTask}>Bring it on!</button>
             </div>
 
-            {/* Displaying the tasks grouped by date */}
-            {Object.entries(todos.reduce((grouped, todo) => {
-                (grouped[todo.date] = grouped[todo.date] || []).push(todo);
-                return grouped;
-            }, {})).map(([date, todosForDate]) => (
+            {/* Search Bar */}
+            <div className="search-bar">
+                <input
+                    type="text"
+                    placeholder="Search tasks"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+
+            {/* Displaying Tasks Grouped by Date */}
+            {Object.entries(groupedTodos).map(([date, todosForDate]) => (
                 <div key={date}>
-                    <h2>{date}</h2>
+                    <h2 className="date-header">{date}</h2>
                     <ul>
                         {todosForDate.map(todo => (
                             <li key={todo.id}>
-                                <span 
-                                    style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                                    onClick={() => setTodos(todos.map(t => 
+                                <span
+                                    className={todo.completed ? 'completed' : ''}
+                                    onClick={() => setTodos(todos.map(t =>
                                         t.id === todo.id ? {...t, completed: !t.completed} : t
                                     ))}
                                 >
-                                    {todo.task}
+                                    {todo.task} (Priority: {todo.priority})
                                 </span>
+                                <div className="task-notes">{todo.notes}</div>
                                 <button onClick={() => setTodos(todos.filter(t => t.id !== todo.id))}>
                                     I'm done with this!
                                 </button>
