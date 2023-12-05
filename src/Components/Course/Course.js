38f2831684d64ebb4091f609df06b729edcbd408
Yuchen Wei <86708505+ez4bk@users.noreply.github.com>
2023-12-04 19:29:18 -0500

    Separate out course nav bar

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,39 +1,19 @@
 import React from 'react';
 import {Breadcrumb, Layout, Menu, theme} from 'antd';
+import {CourseNavBar} from "./CourseNavBar";
 
 
 const {Header, Content, Footer} = Layout;
-
-function getItem(label, key) {
-    return {
-        key,
-        label,
-    };
-}
-
-const items = [
-    getItem('Home', '1'),
-    getItem('Materials', '2'),
-    getItem('Assignments', '3'),
-    getItem('Quizzes', '4'),
-    getItem('Grades', '5'),
-    getItem('Discussion Forum', '6'),
-]
 
 const Course = ({activeMenu}) => {
     return (
         <Layout className="layout">
             <div className="demo-logo"/>
-            <Menu
-                theme="light"
-                mode="horizontal"
-                defaultSelectedKeys={['1']}
-                items={items}
-            />
+            <CourseNavBar/>
             <Content style={{padding: '0 50px',}}>
                 <div className="site-layout-content">
 
-                    {activeMenu}
+                    {/*{activeMenu}*/}
                 </div>
             </Content>
         </Layout>
---
+++ src/Components/Course/CourseNavBar.jsx
@@ -0,0 +1,29 @@
+import {Menu} from "antd";
+import React from "react";
+
+function getItem(label, key) {
+    return {
+        key,
+        label,
+    };
+}
+
+const items = [
+    getItem('Home', '1'),
+    getItem('Materials', '2'),
+    getItem('Assignments', '3'),
+    getItem('Quizzes', '4'),
+    getItem('Grades', '5'),
+    getItem('Discussion Forum', '6'),
+]
+
+export function CourseNavBar() {
+    return (
+        <Menu
+            theme="light"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            items={items}
+        />
+    )
+}
