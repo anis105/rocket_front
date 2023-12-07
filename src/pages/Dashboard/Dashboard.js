c3414b3a765a70438cda54ea51a64c8b5c8138ee
Hanlun Zheng <hanlunzheng@macbook-pro-3.mynetworksettings.com>
2023-12-07 15:22:38 -0500

    Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import {Layout} from 'antd';
-
-
-const {Header, Content, Footer} = Layout;
+import React from "react";
+import { Layout } from "antd";
+import Card from "../../components/card/Card";
+import "./Dashboard.css";
+const { Header, Content, Footer } = Layout;
 
 const Dashboard = () => {
-    return (
-        <Layout className="layout">
-            <div className="demo-logo"/>
-            <Content style={{padding: '0 50px',}}>
-                <div className="site-layout-content">
-                    Dashboard here
-                    {/*{activeMenu}*/}
-                </div>
-            </Content>
-        </Layout>
-    );
+  return (
+    <Layout className="layout">
+      <div className="demo-logo" />
+      <Content style={{ padding: "0 50px" }}>
+        <div className="site-layout-content">
+          <p>Main Dashboard</p>
+          <div className="cardContainer">
+            <Card type="1" />
+            <Card type="2" />
+            <Card type="3" />
+            <Card type="4" />
+          </div>
+          <div className="cardContainer">
+            <Card type="5" />
+            <Card type="6" />
+            <Card type="7" />
+            <Card type="8" />
+          </div>
+          <div className="cardContainer">
+            <Card type="9" />
+            <Card type="10" />
+            <Card type="11" />
+            <Card type="12" />
+          </div>
+        </div>
+      </Content>
+    </Layout>
+  );
 };
 export default Dashboard;
